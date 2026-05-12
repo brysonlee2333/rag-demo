08e5335c9ee3ced249503794b799d39b4986e484
brysonlee2333 <brysonlee233@gmail.com>
2026-05-13 00:39:47 +0800

    文件上传、RAG处理及存储

---
+++ rag_project/app_file_upload.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from knowledge_base import KnowledgeBaseService
+
+# cd命令切换到文件所在目录，执行命令 streamlit run 文件名, 网页访问地址http://localhost:8501
+# 当web页面元素发生变化时，则代码重新执行一遍
+# 添加标题
+st.title("知识库更新服务")
+
+# file upload
+upload_file = st.file_uploader(
+    label="请上传TXT文件",
+    type=["txt"],
+    accept_multiple_files=False
+)
+
+if "service" not in st.session_state:
+    st.session_state["service"] = KnowledgeBaseService()
+
+if upload_file is not None:
+    file_name = upload_file.name
+    file_type = upload_file.type
+    file_size = upload_file.size/1024 #kb
+
+    st.subheader(f"文件名：{file_name}")
+    st.write(f"格式：{file_type}|大小：{file_size:.2f}KB")
+    text = upload_file.getvalue().decode("utf-8")
+    st.session_state["service"].upload_by_str(text, file_name)
---
+++ rag_project/knowledge_base.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+import hashlib
+import os.path
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
+from langchain_community.embeddings import DashScopeEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+import config_data as config
+load_dotenv()
+
+def check_md5(md5_str: str):
+    """
+    检查传入的md5字符串是否已经被处理过，False未处理，True已处理；此方法可避免知识库文件被反复存储
+    """
+    if not os.path.exists(config.md5_path):
+        open(config.md5_path,'w', encoding='utf-8').close() # 此方法会创建文件
+        return False
+    else:
+        with open(config.md5_path, 'r', encoding='utf-8') as f:
+            for line in f.readlines():
+                line = line.strip()
+                if line == md5_str:
+                    return True
+            return False
+
+def save_md5(md5_str: str):
+    """将传入的md5字符串，记录到文件内保存"""
+    with open(config.md5_path, 'a', encoding='utf-8') as f:
+        f.write(md5_str + '\n')
+
+def get_string_md5(input_str: str, encoding='utf-8'):
+    """将传入的字符串转换为md5字符串"""
+    str_bytes = input_str.encode(encoding=encoding)
+    md5_obj = hashlib.md5()
+    md5_obj.update(str_bytes)
+    md5_hex = md5_obj.hexdigest() # 得到16进制的md5字符串
+    return md5_hex
+
+
+class KnowledgeBaseService(object):
+    def __init__(self):
+        # 如果文件不存在则创建，如果存在则跳过
+        os.makedirs(config.persist_directory, exist_ok=True)
+        api_key = os.getenv("DASHSCOPE_API_KEY")
+        self.chroma = Chroma(
+            collection_name=config.collection_name,  # 数据库表名
+            embedding_function=DashScopeEmbeddings(
+                model="text-embedding-v1",
+                dashscope_api_key = api_key),
+            persist_directory=config.persist_directory,
+        )
+
+        self.spliter = RecursiveCharacterTextSplitter(
+            chunk_size=config.chunk_size, # 分割后文本段最大长度
+            chunk_overlap=config.chunk_overlap, # 连续文本段之间的字符重叠数量
+            separators=config.separators, # 自然段落划分的符号
+            length_function=len
+        )
+
+    def upload_by_str(self, data: str, filename: str):
+        """将传入的字符串，进行向量化，存入向量数据库中"""
+        md5_hex = get_string_md5(data)
+
+        if check_md5(md5_hex):
+            return "[跳过]内容已经存在知识库中"
+
+        if len(data) > config.max_split_char_number:
+            knowledge_chunks: list[str] = self.spliter.split_text(data)
+        else:
+            knowledge_chunks = [data]
+
+        metadata = {
+            "source": filename,
+            "create_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "operator": "小曹"}
+
+        self.chroma.add_texts(
+            texts=knowledge_chunks,
+            metadatas=[metadata for _ in knowledge_chunks]
+        )
+
+        save_md5(md5_hex)
+        return "[成功]内容已经成功载入向量库"
+
+if __name__ == "__main__":
+    service = KnowledgeBaseService()
+    r = service.upload_by_str("周杰轮333", "testfile")
+    print(r)
